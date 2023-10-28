5be4e4a85a2924a8c09cebea1703fbfb0ff87563
IvanThankachen <igthankachen@stu.naperville203.org>
2023-10-28 02:07:52 -0500

    10/28/23 - Finished getNeighbors method and changed the new ArrayList list to new ArrayList neighbor because there were complications with declaration that were fixed when another name was used. Also completed getStart(), getEnd(), and reset(). Still trying to verify if the backup maze is the original file or if it will be the alias of the maze array.  Still need to troubleshoot the newly created methods so next goal is to clean up current methods and perform unit test.
    10/28/23 - Finished getNeighbors method and changed the new ArrayList list to new ArrayList neighbor because there were complications with declaration that were fixed when another name was used. Also completed getStart(), getEnd(), and reset(). Still trying to verify if the backup maze is the original file or if it will be the alias of the maze array.  Still need to troubleshoot the newly created methods so next goal is to clean up current methods and perform unit test.

--- src/Maze.java
+++ src/Maze.java
@@ -1,9 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.*;
 
 public class Maze
 {
     
     private Square[][] maze;
+    private Square[][] maze_2 = maze; // In order to reset the maze.
+    private Square start = null;
+    private Square end = null;
 
     public Maze()
     {
@@ -36,12 +42,73 @@ public class Maze
 
     public ArrayList<Square> getNeighbors(Square sq)
     {
-        ArrayList<Square> list = new ArrayList<Square>();
-        
-        for (Square nextSquare : list)
+        ArrayList<Square> neighbor = new ArrayList<Square>();
         {
-            if(s.getRow()  < 0)
+            if (sq.getRow()-1 >= 0) //Condition true as long as row is positive.
+            {
+                neighbor.add(maze[sq.getRow()-1][sq.getCol()]);
+            }
+
+            if(sq.getCol() - 1 >= 0) //Condition true as long as column is positive.
+            {
+                neighbor.add(maze[sq.getRow()][sq.getCol()-1]);
+            }
+
+            if(sq.getRow()+1 < maze.length) //Condition true as long as row doesn't exceed the total amount of rows.
+            {
+                neighbor.add(maze[sq.getRow()+1][sq.getCol()]);
+            }
+
+            if(sq.getCol() + 1 < maze[0].length)  //Condition true as long as column doesn't exceed the total amount of columns.
+            {
+                neighbor.add(maze[sq.getRow()][sq.getCol()+1]);
+            }
+
+            return neighbor;
         }
     }
+
+        public Square getStart()
+        {
+            for (int i = 0, j = 0 ; i < maze.length && j < maze[0].length;i++,j++)
+            {
+                if(maze[i][j].getType() == 2)
+                {
+                    start = new Square (i,j,maze[i][j].getType());
+                }
+
+                
+
+
+            }
+            return start;
+
+            
+        }
+
+        public Square getEnd()
+        {
+            for (int i = 0, j = 0 ; i < maze.length && j < maze[0].length;i++,j++)
+            {
+               if(maze[i][j].getType() == 3)
+                {
+                    end = new Square (i,j,maze[i][j].getType());
+                }
+
+                
+            }
+            return end;
+
+        
+        
+        }
+
+        public void reset()
+        {
+            for(int i = 0, j= 0; i < maze.length && j < maze[0].length; i++,j++)
+            {
+                maze[i][j] = maze_2[i][j];
+            }
+        }
 
 }
