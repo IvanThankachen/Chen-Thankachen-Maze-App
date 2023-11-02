f9a2e58dd948f81df4674d58bd8035b6958c327f
IvanThankachen <111453728+IvanThankachen@users.noreply.github.com>
2023-11-02 13:17:54 -0500

    same as before

---
+++ src/MyQueue.java
@@ -0,0 +1,23 @@
+
+
+public class MyQueue implements QueueADT
+{
+
+    private Node first; 
+    public MyQueue()
+    {
+        first = null;
+    }
+
+
+
+    static class Node()
+    {
+        public Object data;
+        public Node next;
+    }   
+
+
+}
+
+
