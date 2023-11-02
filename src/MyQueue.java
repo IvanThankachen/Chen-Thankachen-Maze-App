75b173f2781c965efa53a7befc2f472ae7d0fbc0
IvanThankachen <111453728+IvanThankachen@users.noreply.github.com>
2023-11-02 13:30:03 -0500

    11/2 - Finished constructor and one method. Will complete the other methods tonight.

--- src/MyQueue.java
+++ src/MyQueue.java
@@ -4,9 +4,19 @@ public class MyQueue implements QueueADT
 {
 
     private Node first; 
+    
+
     public MyQueue()
     {
         first = null;
+        int count = 0;
+    }
+
+
+    public void enqueue(Object element)
+    {
+        Node newNode = new Node();
+        
     }
 
 
