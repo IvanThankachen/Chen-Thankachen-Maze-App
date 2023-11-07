da0a7d0c6d1fb0f80f471badc6cea445d2e588b6
IvanThankachen <igthankachen@stu.naperville203.org>
2023-11-06 23:41:59 -0600

    11/6/23 - Adjusted Queue

--- src/MyQueue.java
+++ src/MyQueue.java
@@ -1,33 +1,72 @@
-
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 public class MyQueue implements QueueADT
 {
 
-    private Node first; 
-    
-
+     private Node first = null;
+     private Node last = null;
+     int count = 0; 
     public MyQueue()
     {
-        first = null;
-        int count = 0;
+       
+
     }
-
-
     public void enqueue(Object element)
     {
         Node newNode = new Node();
-        
+        newNode.data = element;
+        newNode.next = this.first;
+        this.first = newNode;
+        count++;
+
+    }
+
+    public Object dequeue()
+    {
+        if (first == null)
+            throw new  NoSuchElementException();
+            Object prev = first.data;
+            first = first.next;
+            count--;
+            return prev;
+    }
+
+    public Object front()
+    {
+        if (first == null)
+            throw new NoSuchElementException();
+        return first.data;
+    }
+
+    public int size()
+    {
+        return count;
+    }
+
+    public boolean isEmpty()
+    {
+        if (count == 0)
+            return true;
+        else
+            return false;
+    }
+
+    public void clear()
+    {
+        first.data = null;
+        first.next = last;
+        last.data = null;
     }
 
 
 
-    static class Node()
+
+    static class Node
     {
         public Object data;
         public Node next;
-    }   
-
-
+    }
 }
 
 
