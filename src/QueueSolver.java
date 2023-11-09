18b6e0d400a186c430e92d11c91c659695cdfeb2
IvanThankachen <igthankachen@stu.naperville203.org>
2023-11-09 00:48:05 -0600

    11/08 - Finished QueueSolver. Still need to test for bugs, but it should all be structurally good.

---
+++ src/QueueSolver.java
@@ -0,0 +1,38 @@
+public class QueueSolver extends MazeSolver {
+
+    MyQueue worklist;
+    public QueueSolver()
+    {
+        super(m);
+        
+
+    }
+
+    public void makeEmpty()
+    {
+        worklist = new MyQueue();
+        worklist.enqueue(m.getStart());
+
+    }
+
+    public void add (Square sq)
+    {
+        worklist.enqueue(sq);
+    }
+
+    public boolean isEmpty()
+    {
+        if(worklist.size() == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public Square next()
+    {
+        Square nextSquare = (Square) worklist.dequeue();
+        return nextSquare;
+    }
+    
+}
